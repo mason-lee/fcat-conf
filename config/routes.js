6daf1e0ef8c96c6ba3e0a58a2aea61585af5389e
Mason <mason.mideumlee@gmail.com>
2014-10-21 18:20:02 -0700

    Minor routing mistake fixed

--- config/routes.js
+++ config/routes.js
@@ -45,7 +45,7 @@ module.exports.routes = {
 	*                                                                          *
 	***************************************************************************/
 
-	'/register': 'UserController.new',
+	'/submit': 'UserController.new',
 	
 	'/about': {
 		view: 'static/about'
