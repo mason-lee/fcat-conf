46c4e69fe1bb68ca8b5d9a34cc98d1fca1462778
Mason <mason.mideumlee@gmail.com>
2014-11-09 11:32:54 -0800

    ..

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -1,23 +1,3 @@
-// module.exports = function(grunt) {
-// 	grunt.config.set('sass', {
-// 		dev: {
-// 			options: {
-// 				loadPath: require('node-bourbon').includePaths	
-// 			},
-			
-// 			files: [{
-// 				expand: true,
-// 				cwd: 'assets/styles/',
-// 				src: ['*.scss'],
-// 				dest: '.tmp/public/styles/',
-// 				ext: '.css'
-// 			}]
-// 		}
-// 	});
-
-// 	grunt.loadNpmTasks('grunt-contrib-sass');
-// };
-
 module.exports = function(grunt) {
 
     grunt.config.set('sass', {
