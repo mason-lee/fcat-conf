5b19f465e2b2ec20d9f8e49c91ae677cf74fcdb9
Mason <mason.mideumlee@gmail.com>
2015-01-16 11:58:11 -0800

    Image uploading to local disk

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -20,35 +20,35 @@ module.exports = {
             
              // Upload to S3
             res.setTimeout(0);
-            req.file('avatar').upload({
-                adapter: require('skipper-s3'),
-                bucket: process.env.S3_BUCKET,
-                key: process.env.S3_KEY,
-                secret: process.env.S3_SECRET,
-                headers: {
-                    'x-amz-acl': 'public-read'
-                }
-            }, function (err, uploadedFiles) {
-                if(err) return res.send(500, err);
-                // Otherwise, update the user avatar
-                User.update({id: user.id}, {avatar: uploadedFiles[0].extra.Location}).exec(function(err, updated) {
-                    res.redirect("/success");
-                });
-            });
-
-            // req.file('avatar').upload({ adapter: require("skipper-disk"), dirname: '../../assets/images' },function onUploadComplete (err, files) {             
-            //         if (err) return res.send(500, err);
-            //         else {
-            //             var imageFile = files[0].fd;
-            //             var filenameString = JSON.stringify(imageFile);
-            //             var n = filenameString.lastIndexOf("/");
-            //             var result = filenameString.substring(n+1).replace('"', '');
-                        
-            //             User.update({id: user.id}, { avatar: '/images/'+ result }).exec(function(err, updated){
-            //                 res.redirect("/success");
-            //             });
-            //         }
+            // req.file('avatar').upload({
+            //     adapter: require('skipper-s3'),
+            //     bucket: process.env.S3_BUCKET,
+            //     key: process.env.S3_KEY,
+            //     secret: process.env.S3_SECRET,
+            //     headers: {
+            //         'x-amz-acl': 'public-read'
+            //     }
+            // }, function (err, uploadedFiles) {
+            //     if(err) return res.send(500, err);
+            //     // Otherwise, update the user avatar
+            //     User.update({id: user.id}, {avatar: uploadedFiles[0].extra.Location}).exec(function(err, updated) {
+            //         res.redirect("/success");
+            //     });
             // });
+
+            req.file('avatar').upload({ adapter: require("skipper-disk"), dirname: '../../assets/images' },function onUploadComplete (err, files) {             
+                    if (err) return res.send(500, err);
+                    else {
+                        var imageFile = files[0].fd;
+                        var filenameString = JSON.stringify(imageFile);
+                        var n = filenameString.lastIndexOf("/");
+                        var result = filenameString.substring(n+1).replace('"', '');
+                        
+                        User.update({id: user.id}, { avatar: '/images/'+ result }).exec(function(err, updated){
+                            res.redirect("/success");
+                        });
+                    }
+            });
         })//End User.create query
     }//End create action
 };//End module.exports
